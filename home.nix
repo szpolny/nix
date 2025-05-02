5d95e67da5bce670c93956ee940d00edce5d6b55
Szymon <szymonpolny@outlook.com>
2025-05-02 16:57:57 +0200

    Shell configuration

--- home.nix
+++ home.nix
@@ -10,6 +10,10 @@ in
   home.packages = [
     pkgs.git
     pkgs.lazygit
+    pkgs.zsh
+    pkgs.starship
+    pkgs.fzf
+    pkgs.zoxide
   ];
 
   programs.home-manager.enable = true;
@@ -23,6 +27,64 @@ in
   };
 
   programs.lazygit.enable = true;
+
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    history.size = 10000;
+    history.save = 10000;
+    history.ignoreDups = true;
+    history.ignoreAllDups = true;
+    history.saveNoDups = true;
+    history.findNoDups = true;
+    history.share = true;
+    history.append = true;
+    history.path = "$HOME/.zsh_history";
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-syntax-highlighting"; }
+        { name = "zsh-users/zsh-completions"; }
+	{ name = "zsh-users/zsh-autosuggestions"; }
+	{ name = "zsh-users/zsh-history-substring-search"; }
+	{ name = "Aloxaf/fzf-tab"; }
+      ];
+    };
+
+    shellAliases = {
+      reload = "source ~/.zshrc";
+      ls = "ls --color";
+      mkdir = "mkdir -p";
+    };
+
+    initContent = ''
+      bindkey '^B' autosuggest-toggle
+
+      bindkey '^[[A' history-substring-search-up
+      bindkey '^[[B' history-substring-search-down
+
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+      bindkey -e
+    '';
+  };
 
   programs.git = {
     enable = true;
