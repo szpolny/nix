613576b9d11521ead6f106dea1b3429b037a1403
Szymon <szymonpolny@outlook.com>
2025-06-02 13:22:27 +0200

    chore(flakes): bump dependencies

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -1,10 +1,13 @@
 {
   nixpkgs,
   platform,
+  user,
   ...
 }:
 {
   nixpkgs.hostPlatform = "${platform}";
+
+  system.primaryUser = user;
 
   # Call for touchId instead of password, when sudo
   security.pam.services.sudo_local.touchIdAuth = true;
