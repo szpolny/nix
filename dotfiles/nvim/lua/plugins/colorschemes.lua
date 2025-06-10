e7114a3b6740c93aad6b9ffd342bdf8e0f9c98a8
Szymon <szymonpolny@outlook.com>
2025-06-10 13:23:34 +0200

    feat(nvim): add github-nvim-theme and set as default colorscheme
    - Added `projekt0n/github-nvim-theme` to Neovim plugins and set
    `github_dark_default` as the default colorscheme.
    - Commented out previous Gruvbox Material configuration.
    - Updated Kitty terminal theme to "Github_Dark" for consistency.

--- dotfiles/nvim/lua/plugins/colorschemes.lua
+++ dotfiles/nvim/lua/plugins/colorschemes.lua
@@ -9,6 +9,12 @@ return {
   {
     'rebelot/kanagawa.nvim',
   },
+  {
+    'projekt0n/github-nvim-theme',
+    config = function()
+      vim.cmd.colorscheme 'github_dark_default'
+    end,
+  },
   { 'savq/melange-nvim' },
   { 'vague2k/vague.nvim', opts = {} },
   {
@@ -17,15 +23,15 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.gruvbox_material_background = 'hard'
-      vim.g.gruvbox_material_foreground = 'mix'
-      vim.g.gruvbox_material_ui_contrast = 'hard'
-      vim.g.gruvbox_material_float_style = 'bright'
-      vim.g.gruvbox_material_statusline_style = 'material'
-
-      vim.g.gruvbox_material_better_performance = 1
-
-      vim.cmd.colorscheme 'gruvbox-material'
+      -- vim.g.gruvbox_material_background = 'hard'
+      -- vim.g.gruvbox_material_foreground = 'mix'
+      -- vim.g.gruvbox_material_ui_contrast = 'hard'
+      -- vim.g.gruvbox_material_float_style = 'bright'
+      -- vim.g.gruvbox_material_statusline_style = 'material'
+      --
+      -- vim.g.gruvbox_material_better_performance = 1
+      --
+      -- vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 }
