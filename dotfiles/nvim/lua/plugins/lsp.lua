aee9b624067ef504184ebc3bebc07f2dcae350c8
Szymon <szymonpolny@outlook.com>
2025-06-20 12:23:36 +0200

    added xml lsp

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   {
     'mason-org/mason-lspconfig.nvim',
-    opts = { ensure_installed = { 'lua_ls', 'rust_analyzer' } },
+    opts = { ensure_installed = { 'lua_ls', 'rust_analyzer', 'lemminx' } },
     dependencies = {
       { 'mason-org/mason.nvim', opts = {} },
       'neovim/nvim-lspconfig',
