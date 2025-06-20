1c4ea5614d877cd0e5eed82a03b7a2f948a0d697
Szymon <szymonpolny@outlook.com>
2025-06-20 13:06:25 +0200

    Some tweaks in lsp

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,57 @@
 return {
   {
-    'mason-org/mason-lspconfig.nvim',
-    opts = { ensure_installed = { 'lua_ls', 'rust_analyzer', 'lemminx' } },
+    'neovim/nvim-lspconfig',
     dependencies = {
-      { 'mason-org/mason.nvim', opts = {} },
-      'neovim/nvim-lspconfig',
+      { 'mason-org/mason.nvim' },
+      'mason-org/mason-lspconfig.nvim',
       'j-hui/fidget.nvim',
+      'saghen/blink.cmp',
     },
+    config = function()
+      vim.diagnostic.config {
+        severity_sort = true,
+        float = { border = 'rounded', source = 'if_many' },
+        underline = { severity = vim.diagnostic.severity.ERROR },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
+          },
+        },
+        virtual_text = {
+          source = 'if_many',
+          spacing = 2,
+          format = function(diagnostic)
+            local diagnostic_message = {
+              [vim.diagnostic.severity.ERROR] = diagnostic.message,
+              [vim.diagnostic.severity.WARN] = diagnostic.message,
+              [vim.diagnostic.severity.INFO] = diagnostic.message,
+              [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+          end,
+        },
+      }
+
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+      require('mason').setup()
+      require('mason-lspconfig').setup {
+        automatic_enable = true,
+        ensure_installed = {
+          'lua_ls',
+          'rust_analyzer',
+          'lemminx',
+        },
+        handler = function(server_name)
+          require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+          }
+        end,
+      }
+    end,
   },
   {
     'folke/lazydev.nvim',
